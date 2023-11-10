bfaeef2414d5c96dcc572823bc09ac470a0ecf2a
Pol <politronqui@gmail.com>
2023-11-10 23:24:35 +0100

    archiu sense main

--- ProgramaSeseMain.c
+++ ProgramaSeseMain.c
@@ -73,13 +73,13 @@ float Scalar( float vect1[N], float vect2[N] ){
 }
 float Magnitude( float vect[N] ){
         float res = 0;
-	for(int i =0; i<N;i++){
-		float suma = 0;
-		suma = pow(vect[i],2);
-		res +=suma;
-	}
-	float magnitude = sqrt(res);
-	return magnitude;
+        for(int i =0; i<N;i++){
+                float suma = 0;
+                suma = pow(vect[i],2);
+                res +=suma;
+        }
+        float magnitude = sqrt(res);
+        return magnitude;
 }
 int Ortogonal( float vect1[N], float vect2[N] ){
         float resp;
@@ -163,8 +163,8 @@ int Jacobi( float M[N][N] , float vect[N], float vectres[N], unsigned iter ){
         if (DiagonalDom(M)==0) {
                 printf("La matriu no és diagonal dominant, per tant, no es pot aplicar la funció Jacobi");
                 return 0;
-	}
-	for (unsigned k = 0; k < iter; k++) {
+        }
+        for (unsigned k = 0; k < iter; k++) {
         float temp[N];
 
         for (int i = 0; i < N; i++) {
@@ -188,121 +188,4 @@ int Jacobi( float M[N][N] , float vect[N], float vectres[N], unsigned iter ){
     return 1;
 }
 int main(){
-        InitData();
-
-    // Llama a todas las demás funciones aquí
-    PrintVect(V1, 0, 10);
-    printf("\n");
-    PrintVect(V1,  256, 10);
-    printf("\n");
-    PrintVect(V2, 0, 10);
-    printf("\n");
-    PrintVect(V2, 256, 10);
-    printf("\n");
-    PrintVect(V3, 0, 10);
-    printf("\n");
-    PrintVect(V3, 256, 10);
-    printf("\n");
-    PrintRow(Mat, 0, 0, 10);
-    printf("\n");
-    PrintRow(Mat, 100, 0, 10);
-    printf("\n");
-    PrintRow(MatDD, 0, 0, 10);
-    printf("\n");
-    PrintRow(MatDD, 100, 90, 10);
-    printf("\n");
-
-    float inf, one, frob;
-    inf=Infininorm(Mat);
-    printf("Infininorm de Mat: %f\n", inf);
-    printf("\n");
-    one=Onenorm(Mat);
-    printf("Onenorm de Mat: %f\n", one);
-    printf("\n");
-    frob=NormFrobenius(Mat);
-    printf("Frobenius de Mat: %f\n", frob);
-    printf("\n");
-    int isDiagonalDom = DiagonalDom(Mat);
-    if (isDiagonalDom) {
-        printf("Mat és diagonal dominant.\n");
-        printf("\n");
-    } else {
-        printf("Mat no és diagonal dominant.\n");
-        printf("\n");
-    }
-    inf=Infininorm(MatDD);
-    printf("Infininorm de MatDD: %f\n", inf);
-    printf("\n");
-    one=Onenorm(MatDD);
-    printf("Onenorm de MatDD: %f\n", one);
-    printf("\n");
-    frob=NormFrobenius(MatDD);
-    printf("Frobenius de MatDD %f\n", frob);
-    printf("\n");
-    int isDiagonalDomDD = DiagonalDom(MatDD);
-    if (isDiagonalDomDD) {
-        printf("MatDD és diagonal dominant.\n");
-        printf("\n");
-    } else {
-        printf("MatDD no és diagonal dominant.\n");
-        printf("\n");
-    }
-
-    float result = Scalar(V1, V2);
-    printf("Ecalar V1 V2: %f\n", result);
-    printf("\n");
-    float result1 = Scalar(V1, V3);
-    printf("Escalar V1 V3: %f\n", result1);
-    printf("\n");
-    float result2 = Scalar(V2, V3);
-    printf("Escalar V2 V3: %f\n", result2);
-    printf("\n");
-    result = Magnitude(V1);
-    printf("Magnitud de V1: %f\n", result);
-    printf("\n");
-    result = Magnitude(V2);
-    printf("Magnitud de V2: %f\n", result);
-    printf("\n");
-    result = Magnitude(V3);
-    printf("Magnitud de V3: %f\n", result);
-    printf("\n");
-
-    int isOrthogonal = Ortogonal(V1, V2);
-    if (isOrthogonal) {
-        printf("V1 y V2 són ortogonals.\n");
-        printf("\n");
-    } else {
-        printf("V1 y V2 no són ortogonals.\n");
-        printf("\n");
-    }
-
-    MultEscalar(V3, V3, 2.0);
-    PrintVect(V3, 0, 10);
-    printf("\n");
-    PrintVect(V3, 256, 10);
-    printf("\n");
-
-    Projection(V2, V3, V4);
-    PrintVect(V4, 0 ,10);
-    printf("\n");
-    Projection(V1, V2, V4);
-    PrintVect(V4, 0, 10);
-    printf("\n");
-        // J Jacob
-    Jacobi(MatDD,V3,V4,1);
-    printf("ELs elements 0 a 9 de la solució (1 iter) del sistema d'equacions són: \n");
-    for (int i = 0; i < 10; i++){
-            printf("%f ", V4[i]);
-                }
-    printf("\n");
-    Jacobi(MatDD,V3,V4,1000);
-    printf("ELs elements 0 a 9 de la solució (1000 iter) del sistema d'equacions són: ");
-            for (int i = 0; i < 10; i++){
-            printf("%f ", V4[i]);
-                }
-    printf("\n");
-
-    // Llama a las demás funciones aquí
-
-    return 0;
 }
