5dcf7c172b502e581dc34d0cb91b4952fe4d588e
Pol <politronqui@gmail.com>
2023-11-10 12:43:49 +0100

    Init data cambiat

--- p2.c
+++ p2.c
@@ -10,7 +10,7 @@ float V3[N];
 float V4[N];
 void InitData(){
         int i,j;
-        srand(8824553);
+        srand(4422543);
         for( i = 0; i < N; i++ )
                 for( j = 0; j < N; j++ ){
                         Mat[i][j]=(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX)));
